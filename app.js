cdeb53efd2decafdc3b5919651724570ad699714
mgalvan <tech.mgalvan@gmail.com>
2025-03-18 19:52:27 -0600

    inicializando respositorio

---
+++ app.js
@@ -0,0 +1,23 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const NombresAmigos = document.getElementById ("amigo");
+const ListadoAmigos = [];
+const ListaAmigos = document.getElementById ("listaAmigos");
+const Asecreto = document.getElementById("resultado");
+
+function agregarAmigo() {
+    
+    if (!NombresAmigos.value){
+        alert("Escribe un nombre valido")
+    }
+
+    ListadoAmigos.push(NombresAmigos.value);
+    ListaAmigos.innerHTML += `<li>${NombresAmigos.value}</li>`;
+
+};
+
+function sortearAmigo(){
+    const random = Math.floor(Math.random()*ListadoAmigos.length);
+    const amigoSecreto = ListadoAmigos[random];
+    Asecreto.innerHTML= `<li>Tu amigo secreto es:${amigoSecreto}</li>`;
+};
+
